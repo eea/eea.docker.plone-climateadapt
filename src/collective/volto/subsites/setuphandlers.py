1a68b50873b9e1c8f6a9a3de708a69b1f1f69050
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-04-29 17:39:36 +0200

    Disable Subsite from searchable contents

--- src/collective/volto/subsites/setuphandlers.py
+++ src/collective/volto/subsites/setuphandlers.py
@@ -1,21 +1,33 @@
 # -*- coding: utf-8 -*-
+from plone.registry.interfaces import IRegistry
 from Products.CMFPlone.interfaces import INonInstallable
+from Products.CMFPlone.interfaces import ISearchSchema
+from zope.component import getUtility
 from zope.interface import implementer
 
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
-
     def getNonInstallableProfiles(self):
         """Hide uninstall profile from site-creation and quickinstaller."""
         return [
-            'collective.volto.subsites:uninstall',
+            "collective.volto.subsites:uninstall",
         ]
 
 
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+    disable_searchable_type()
+
+
+def disable_searchable_type():
+    # remove Subsite from searchable types
+    registry = getUtility(IRegistry)
+    settings = registry.forInterface(ISearchSchema, prefix="plone")
+
+    types = list(settings.types_not_searched) + ["Subsite"]
+    settings.types_not_searched = tuple(types)
 
 
 def uninstall(context):
